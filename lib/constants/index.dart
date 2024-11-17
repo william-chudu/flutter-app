3a72144b9618bb312a11b7a74347f2f13975add5
William Vux <chungvux163@gmail.com>
2024-11-17 18:41:39 +0700

    commit new code

--- lib/constants/index.dart
+++ lib/constants/index.dart
@@ -13,7 +13,7 @@ final class AppConstants {
   LanguageLabel get label => _isEn ? _lang.enLabel : _lang.vnLabel;
   ELocale get locale => _isEn ? ELocale.en : ELocale.vn;
   static DefaultLink link = const DefaultLink();
-  static final Color accent = Colors.deepOrange.shade900;
+  static const Color accent = Color(0xFFBF360C);
 
   void setLanguage([bool isEn = false]) {
     _isEn = isEn;
